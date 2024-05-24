344373df4489d2c5263f278dd25e51b60813e1b6
ziyak8856 <ziyak7710@gmail.com>
2024-05-24 23:23:58 +0530

    pipeline

--- api/app.js
+++ api/app.js
@@ -10,7 +10,12 @@ import messageRoute from "./routes/message.route.js";
 
 
 const app = express();
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
