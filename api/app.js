267af098ea523bad1700c8c18e676e8103953cbf
ziyak8856 <ziyak7710@gmail.com>
2024-05-25 10:58:37 +0530

    m

--- api/app.js
+++ api/app.js
@@ -10,18 +10,18 @@ import messageRoute from "./routes/message.route.js";
 
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-   // Allow requests from the frontend running on this origin
-}));
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+// // app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+//    // Allow requests from the frontend running on this origin
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
