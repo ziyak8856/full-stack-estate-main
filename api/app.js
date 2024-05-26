60742e21065cf5668cf2b9c64f261465e92cd387
ziyak8856 <ziyak7710@gmail.com>
2024-05-26 16:21:37 +0530

    cros

--- api/app.js
+++ api/app.js
@@ -15,7 +15,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 // CORS configuration
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+app.use('/profile/add', cors({
+  origin: 'http://localhost:5173', // Specify the allowed origin for /profile/add route
+  credentials: true
+}));
 
 
 // Routes
